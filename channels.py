79950f0dc071fac51b18eab074aaf756ea39c5eb
vinney560 <vinneyjoy1@gmail.com>
2025-07-06 18:18:38 +0300

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,66 +1,66 @@
 CUSTOM_CHANNELS = {
     "citizen-tv": {
         "name": "Citizen TV Kenya",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UChBQgieUidXV1CmDxSdRm3g"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UChBQgieUidXV1CmDxSdRm3g&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "ktn-news": {
         "name": "KTN News Kenya",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "ntv-kenya": {
         "name": "NTV Kenya",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCfY4WfXqkz0A9xDNJchpOAQ"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCfY4WfXqkz0A9xDNJchpOAQ&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "k24-tv": {
         "name": "K24 TV",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCq6cQZAlKbhMXgk4W7yNH6A"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCq6cQZAlKbhMXgk4W7yNH6A&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "tv47-kenya": {
         "name": "TV47 Kenya",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UC7uEaZ9tjO-Q7qJ_gqM9jjA"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UC7uEaZ9tjO-Q7qJ_gqM9jjA&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "churchill-show": {
         "name": "Churchill Show",
-        "url": "https://www.youtube.com/embed/0oJAwOLRGfQ"
+        "url": "https://www.youtube.com/embed/0oJAwOLRGfQ?controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "jalango-tv": {
         "name": "Jalang'o TV",
-        "url": "https://www.youtube.com/embed/3OD2scZsVt4"
+        "url": "https://www.youtube.com/embed/3OD2scZsVt4?controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "mpasho-tv": {
         "name": "Mpasho TV",
-        "url": "https://www.youtube.com/embed/6jV6_Y93_Tc"
+        "url": "https://www.youtube.com/embed/6jV6_Y93_Tc?controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "wasafi-tv": {
         "name": "Wasafi TV",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UC74QOAN2DJpcyZx_jruoWOA"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UC74QOAN2DJpcyZx_jruoWOA&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "kibata-kenya": {
         "name": "Kibata Kenya",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UC46zGIE98A5Vnwi2-I6c1Dw"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UC46zGIE98A5Vnwi2-I6c1Dw&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "urban-street-254": {
         "name": "Urban Street +254",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UC2Q4z3z4fLJc7k5J3a5V5A"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UC2Q4z3z4fLJc7k5J3a5V5A&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "kbc": {
         "name": "Kenya Broadcasting Corporation (KBC)",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCpl2xr6WWfxa2v2X6tqRmIw"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCpl2xr6WWfxa2v2X6tqRmIw&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "kenya-news-agency": {
         "name": "Kenya News Agency",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCaPmFHOiHht_yjXDMnTxlIA"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCaPmFHOiHht_yjXDMnTxlIA&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "nairobi-news": {
         "name": "Nairobi News",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCZvTzdyYl9axT4m_BZ3bqMg"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCZvTzdyYl9axT4m_BZ3bqMg&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "ktn-home": {
         "name": "KTN Home",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCyo9yshD7ERrIr1kFJ38ufQ"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCyo9yshD7ERrIr1kFJ38ufQ&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     },
     "capital-tv-kenya": {
         "name": "Capital TV Kenya",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCkBqL-cGRcSLO-9h8k6dHHA"
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCkBqL-cGRcSLO-9h8k6dHHA&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&iv_load_policy=3"
     }
 }

6802b679c76ec92569e587f77c9b5dbeafd31adc
VINNEY560 <vinneyjoy1@gmail.com>
2025-07-07 19:19:52 +0300

    General Updates

--- channels.py
+++ channels.py
@@ -8,8 +8,8 @@ CUSTOM_CHANNELS = {
         "url": "https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
     },
     "ntv-kenya": {
-        "name": "NTV Live Kenya",
-        "url": "https://www.youtube.com/embed/p4BWnK2xeHw?autoplay=1&controls=1&modestbranding=1&rel=0&showinfo=0"
+        "name": "NTV Kenya",
+        "url": "https://ntvkenya-live.stream/ntvkenya.m3u8"
     },
     "k24-tv": {
         "name": "K24 TV",
@@ -27,17 +27,44 @@ CUSTOM_CHANNELS = {
         "name": "Inooro TV (Kikuyu)",
         "url": "https://www.youtube.com/embed/P3VPIjl_pl0?autoplay=1&controls=1&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
     },
-    "ntv-kenya": {
-        "name": "NTV Kenya",
-        "url": "https://ntvkenya-live.stream/ntvkenya.m3u8"
-    },
-    # Alternative versions / YouTube live streams:
-    "france24-english-youtube": {
+    "france24-english": {
         "name": "France 24 English",
         "url": "https://live.france24.com/hls/live/2037218-b/F24_EN_HI_HLS/master_5000.m3u8"
     },
-    "al-jazeera-english-youtube": {
+    "al-jazeera-english": {
         "name": "Al Jazeera English",
         "url": "https://live-hls-apps-aje-v3-fa.getaj.net/AJE/index.m3u8"
+    },
+    "pluto-tv-action": {
+        "name": "Pluto TV Action",
+        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5e6e2a9342931d0007b14e4a/master.m3u8"
+    },
+    "pluto-tv-drama": {
+        "name": "Pluto TV Drama",
+        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5e6e2b0f42931d0007b14e4b/master.m3u8"
+    },
+    "vevo-pop": {
+        "name": "Vevo Pop",
+        "url": "https://dai.google.com/linear/hls/event/4i9jsoDGShqeqRxfrSmtfQ/master.m3u8"
+    },
+    "vevo-hiphop": {
+        "name": "Vevo Hip-Hop",
+        "url": "https://dai.google.com/linear/hls/event/sRgt7tdNRiC5rUgJIBZaYw/master.m3u8"
+    },
+    "musicbox-eu": {
+        "name": "Music Box (EU)",
+        "url": "http://live.musicboxgroup.ru:8081/musicboxhd/tracks-v1/mono.m3u8"
+    },
+    "power-tv-africa": {
+        "name": "Power TV (Africa)",
+        "url": "http://stream.powersat.tv:1935/powertv/live/playlist.m3u8"
+    },
+    "afromovies": {
+        "name": "Afromovies",
+        "url": "http://afrostreaming.online:8080/live/afromovies.m3u8"
+    },
+    "gospel-music": {
+        "name": "Gospel Music",
+        "url": "https://edge6.clouding.host/live/gospelstream.m3u8"
     }
-}    
+}

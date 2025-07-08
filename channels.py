4d31f810a3dade08346b13bf00a4117bd46f54ed
VINNEY560 <vinneyjoy1@gmail.com>
2025-07-08 17:27:27 +0300

    General Updates

--- channels.py
+++ channels.py
@@ -1,140 +1,4 @@
 CUSTOM_CHANNELS = {
-    "citizen-tv": {
-        "name": "Citizen TV",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UChBQgieUidXV1CmDxSdRm3g&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
-    },
-    "ktn-news": {
-        "name": "KTN News",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
-    },
-    "ntv-kenya": {
-        "name": "NTV",
-        "url": "https://ntvkenya-live.stream/ntvkenya.m3u8"
-    },
-    "k24-tv": {
-        "name": "K24 TV",
-        "url": "https://www.youtube.com/embed/_lB9oSok1FM?controls=1&modestbranding=1&rel=0&fs=1&disablekb=0&iv_load_policy=3"
-    },
-    "fifa": {
-        "name": "Fifa +",
-        "url": "https://a62dad94.wurl.com/master/f36d25e7e52f1ba8d7e56eb859c636563214f541/UmFrdXRlblRWLWV1X0ZJRkFQbHVzRW5nbGlzaF9ITFM/playlist.m3u8"
-    },
-    "france24-english": {
-        "name": "France 24 English",
-        "url": "https://live.france24.com/hls/live/2037218-b/F24_EN_HI_HLS/master_5000.m3u8"
-    },
-    "al-jazeera-english": {
-        "name": "Al Jazeera English",
-        "url": "https://live-hls-apps-aje-v3-fa.getaj.net/AJE/index.m3u8"
-    },
-    "pluto-tv-sports": {
-        "name": "Pluto TV Sports",
-        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5f2aa5fce6fa5200070d707b/master.m3u8"
-    },
-    "action-sports": {
-        "name": "Action Sports",
-        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5f2aa60650e5940007e6081d/master.m3u8"
-    },
-    "tubi-sports": {
-        "name": "Tubi Sports",
-        "url": "https://tubitv.com/live/692524"
-    },
-    "anime-all-day": {
-        "name": "Anime All Day",
-        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/603f3dcaa959e70007806d5c/master.m3u8"
-    },
-    "anime-247": {
-        "name": "Anime 24/7",
-        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5e7d178be6fa520007e28fd4/master.m3u8"
-    },
-    "nick-anime": {
-        "name": "Nick",
-        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/6266b182b8d2fc00078d12d3/master.m3u8"
-    },
-    "pltv-usa-movies": {
-        "name": "Movies",
-        "url": "https://i.mjh.nz/PlutoTV/us.m3u8"
-    },
-    "disney_1": {
-        "name": "Disney",
-        "url": "http://168.181.122.234:8000/play/a037/index.m3u8"
-    },
-    "dusney_2": {
-        "name": "Disney",
-        "url": "https://fl2.moveonjoy.com/DISNEY_JR/index.m3u8"
-    },    
-    "nhk-world-japan": {
-        "name": "NHK World Japan",
-        "url": "https://nhkwlive-ojp.akamaized.net/hls/live/2003459/nhkwlive-ojp-en/index.m3u8"
-    },
-    "nickelodeon": {
-        "name": "Nickelodeon",
-        "url": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5d8d08395f39465da6fb3ec4/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5107-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=919bc5fd-6cce-44a6-bb39-2894dea1c988"
-    },
-    "nick_jr": {
-        "name": "Nick Jr.",
-        "url": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5ca6748a37b88b269472dad9/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5106-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&sid=d95e31ff-c65d-438c-9466-ddbbd097b4cf&serverSideAds=false"
-    },
-    "apostolictv": {
-      "name": "Apostolic Faith Church TV",
-      "url": "https://afctvonline.co.ke/live/"
-    },
-    "oracletv": {
-      "name": "Oracle TV Kenya",
-      "url": "https://oracletv.co.ke/live/"
-    },
-    "kameme": {
-      "name": "Kameme TV",
-      "url": "https://kamemetv.co.ke/live/"
-    },
-    "msenangu": {
-      "name": "Msenangu TV",
-      "url": "https://msenangu.co.ke/live/"
-    },
-    "lolwetv": {
-      "name": "Lolwe TV",
-      "url": "https://lolwetv.co.ke/live/"
-    },
-    "werutv": {
-      "name": "Weru TV",
-      "url": "https://werutv.co.ke/live/"
-    },
-    "nyotatv": {
-      "name": "Nyota TV",
-      "url": "https://nyotatv.co.ke/live/"
-    },
-    "kasstv": {
-      "name": "Kass TV",
-      "url": "https://kasstv.co.ke/live/"
-    },
-    "mbaitutv": {
-      "name": "Mbaitu TV",
-      "url": "https://mbaitutv.co.ke/live/"
-    },
-    "ruath": {
-      "name": "RUATH TV",
-      "url": "https://ruath.tv/live/"
-    },
-    "shifutv": {
-      "name": "Shifu TV",
-      "url": "https://shifutv.com/live/"
-    },
-    "merutv": {
-      "name": "Meru TV",
-      "url": "https://merutv.co.ke/live/"
-    },
-    "akilikids": {
-      "name": "Akili Kids! TV",
-      "url": "https://akilikids.co.ke/live/"
-    },
-    "edu": {
-      "name": "Edu TV",
-      "url": "https://kicd.ac.ke/edutv-live/"
-    },
-    "tvet": {
-      "name": "TVET TV",
-      "url": "https://tvet.tv/live/"
-    },
     "abc_news": {
         "name": "ABC News Live",
         "url": "https://abcnews.go.com/Live"
@@ -143,45 +7,45 @@ CUSTOM_CHANNELS = {
         "name": "Akili Kids",
         "url": "https://www.youtube.com/channel/UCHBIm_ITwYy_FG9iIfIZxGQ"
     },
+    "akilikids": {
+        "name": "Akili Kids! TV",
+        "url": "https://akilikids.co.ke/live/"
+    },
+    "al-jazeera-english": {
+        "name": "Al Jazeera English",
+        "url": "https://live-hls-apps-aje-v3-fa.getaj.net/AJE/index.m3u8"
+    },
     "al_jazeera": {
         "name": "Al Jazeera English",
         "url": "https://www.aljazeera.com/live"
     },
-    "azam_tv": {
-        "name": "Azam TV",
-        "url": "https://azamtv.co.tz/"
-    },
-    "bein_sports": {
-        "name": "beIN SPORTS",
-        "url": "https://www.beinsports.com/en/"
+    "anime-247": {
+        "name": "Anime 24/7",
+        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5e7d178be6fa520007e28fd4/master.m3u8"
     },
     "baby_tv": {
         "name": "Baby TV",
         "url": "https://www.youtube.com/user/BabyTVChannel"
     },
-    "babyfirst": {
-        "name": "BabyFirst TV",
-        "url": "https://www.babyfirsttv.com/watch-live/"
-    },
-    "bloomberg": {
-        "name": "Bloomberg TV",
-        "url": "https://www.bloomberg.com/live/us"
+    "bein_sports": {
+        "name": "beIN SPORTS",
+        "url": "https://www.beinsports.com/en/"
     },
     "bunge_tv": {
         "name": "Bunge TV",
         "url": "https://www.parliament.go.ke/the-national-assembly/live"
     },
-    "cartoon_network": {
-        "name": "Cartoon Network",
-        "url": "https://www.cartoonnetwork.com/video/index.html"
-    },
     "cbs_news": {
         "name": "CBS News",
         "url": "https://www.cbsnews.com/live/"
     },
-    "citizen": {
-        "name": "Citizen Digital",
-        "url": "https://kenyalivetv.co.ke/embed/citizen-tv-live?autoplay=1&mute=1"
+    "citizen-tv": {
+        "name": "Citizen TV",
+        "url": "https://www.youtube.com/embed/live_stream?channel=UChBQgieUidXV1CmDxSdRm3g&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
+    },
+    "citizen-tv-extra": {
+        "name": "Citizen Extra",
+        "url": "https://www.youtube.com/embed/MDA0XHrnNl4"
     },
     "cnn_international": {
         "name": "CNN International",
@@ -191,13 +55,21 @@ CUSTOM_CHANNELS = {
         "name": "DAZN",
         "url": "https://www.dazn.com/en-US/live"
     },
+    "disney_1": {
+        "name": "Disney",
+        "url": "http://168.181.122.234:8000/play/a037/index.m3u8"
+    },
+    "dusney_2": {
+        "name": "Disney",
+        "url": "https://fl2.moveonjoy.com/DISNEY_JR/index.m3u8"
+    },
     "dw_tv": {
         "name": "DW-TV",
         "url": "https://www.dw.com/en/tv/live"
     },
-    "ebru_tv": {
-        "name": "Ebru TV",
-        "url": "https://www.squidtv.net/africa/kenya/kenya-004.html"
+    "edu": {
+        "name": "Edu TV",
+        "url": "https://kicd.ac.ke/edutv-live/"
     },
     "elimu_tv": {
         "name": "Elimu TV",
@@ -211,9 +83,9 @@ CUSTOM_CHANNELS = {
         "name": "Euronews",
         "url": "https://www.euronews.com/live"
     },
-    "family_tv": {
-        "name": "Family TV",
-        "url": "https://www.squidtv.net/africa/kenya/kenya-005.html"
+    "fifa": {
+        "name": "Fifa +",
+        "url": "https://a62dad94.wurl.com/master/f36d25e7e52f1ba8d7e56eb859c636563214f541/UmFrdXRlblRWLWV1X0ZJRkFQbHVzRW5nbGlzaF9ITFM/playlist.m3u8"
     },
     "fifa_plus": {
         "name": "FIFA+",
@@ -223,33 +95,41 @@ CUSTOM_CHANNELS = {
         "name": "France 24 English",
         "url": "https://www.france24.com/en/live"
     },
-    "fuma_tv": {
-        "name": "Fuma TV",
-        "url": "https://www.squidtv.net/africa/kenya/kenya-005.html"
-    },
-    "githima_tv": {
-        "name": "Githima TV",
-        "url": "https://www.squidtv.net/africa/kenya/kenya-004.html"
-    },
-    "hadi_tv": {
-        "name": "Hadi TV",
-        "url": "https://en.haditv.co.uk/"
+    "france24-english": {
+        "name": "France 24 English",
+        "url": "https://live.france24.com/hls/live/2037218-b/F24_EN_HI_HLS/master_5000.m3u8"
     },
     "horizon_tv": {
         "name": "Horizon TV Kenya",
         "url": "https://kenyalivetv.co.ke/tv/"
     },
-    "inooro": {
-        "name": "Inooro TV",
-        "url": "https://kenyalivetv.co.ke/embed/inooro-tv-live?autoplay=1&mute=1"
+    "inoorotv": {
+        "name": "Inooro Tv(CitizenDigital)",
+        "url": "https://citizen.digital/tv/inooro-tv-14"
     },
-    "jimjam": {
-        "name": "JimJam",
-        "url": "https://www.youtube.com/user/JimJamTVChannel"
+    "kameme": {
+        "name": "Kameme TV",
+        "url": "https://kamemetv.co.ke/live/"
     },
-    "kamba_tv": {
-        "name": "Kamba TV",
-        "url": "https://kenyalivetv.co.ke/tv/"
+    "kasstv": {
+        "name": "Kass TV",
+        "url": "https://www.youtube.com/embed/4smx4SyYMbc"
+    },
+    "k24-tv": {
+        "name": "K24 TV",
+        "url": "https://www.youtube.com/embed/_lB9oSok1FM?controls=1&modestbranding=1&rel=0&fs=1&disablekb=0&iv_load_policy=3"
+    },
+    "ktn-news": {
+        "name": "KTN News",
+        "url": "https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
+    },
+    "lolwetv": {
+        "name": "Lolwe TV",
+        "url": "https://lolwetv.co.ke/live/"
+    },
+    "merutv": {
+        "name": "Meru TV",
+        "url": "https://merutv.co.ke/live/"
     },
     "mtv_base": {
         "name": "MTV Base Africa",
@@ -263,6 +143,22 @@ CUSTOM_CHANNELS = {
         "name": "NBC Sports",
         "url": "https://www.nbcsports.com/live"
     },
+    "nhk-world-japan": {
+        "name": "NHK World Japan",
+        "url": "https://nhkwlive-ojp.akamaized.net/hls/live/2003459/nhkwlive-ojp-en/index.m3u8"
+    },
+    "nick-anime": {
+        "name": "Nick",
+        "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/6266b182b8d2fc00078d12d3/master.m3u8"
+    },
+    "nick_jr": {
+        "name": "Nick Jr.",
+        "url": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5ca6748a37b88b269472dad9/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5106-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&sid=d95e31ff-c65d-438c-9466-ddbbd097b4cf&serverSideAds=false"
+    },
+    "nickelodeon": {
+        "name": "Nickelodeon",
+        "url": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5d8d08395f39465da6fb3ec4/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5107-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=919bc5fd-6cce-44a6-bb39-2894dea1c988"
+    },
     "nickelodeon": {
         "name": "Nickelodeon (Pluto TV)",
         "url": "https://pluto.tv/en/live-tv/nick-classics"
@@ -271,52 +167,28 @@ CUSTOM_CHANNELS = {
         "name": "NTV Digital",
         "url": "https://ntvkenya.co.ke/live/"
     },
-    "parliament_tv": {
-        "name": "Parliament TV (PBU)",
-        "url": "https://www.parliament.go.ke/the-national-assembly/live"
+    "ntv-kenya": {
+        "name": "NTV",
+        "url": "https://www.youtube.com/embed/p4BWnK2xeHw"
     },
-    "pbs_kids": {
-        "name": "PBS Kids",
-        "url": "https://pbskids.org/video/"
+    "oracletv": {
+        "name": "Oracle TV Kenya",
+        "url": "https://oracletv.co.ke/live/"
     },
-    "ptv_world": {
-        "name": "PTV World",
-        "url": "https://ptv.com.pk/ptvWorld"
+    "pltv-usa-movies": {
+        "name": "Movies",
+        "url": "https://i.mjh.nz/PlutoTV/us.m3u8"
     },
-    "ramogi": {
-        "name": "Ramogi TV",
-        "url": "https://kenyalivetv.co.ke/embed/ramogi-tv-live?autoplay=1&mute=1"
+    "ramogifmtv": {
+        "name": "Ramogi Fm Tv",
+        "url": "https://citizen.digital/tv/ramogi-fm-audio-visual-24"
     },
-    "raia_tv": {
-        "name": "Raia TV",
-        "url": "https://kenyalivetv.co.ke/tv/"
+    "ramogitv": {
+        "name": "Ramogi Tv",
+        "url": "https://citizen.digital/tv/ramogi-tv-19"
     },
-    "senate_tv": {
-        "name": "Senate TV",
-        "url": "https://www.parliament.go.ke/the-senate/live"
-    },
-    "sky_news": {
-        "name": "Sky News",
-        "url": "https://news.sky.com/watch-live"
-    },
-    "sky_sports": {
-        "name": "Sky Sports",
-        "url": "https://www.skysports.com/watch"
-    },
-    "talksport": {
-        "name": "talkSPORT",
-        "url": "https://talksport.com/boxing/3261089/fabio-wardley-vs-justis-huni-live-commentary-stream-date-start-time-undercard-tv-channel/"
-    },
-    "trace_africa": {
-        "name": "Trace Africa",
-        "url": "https://trace.tv/trace-africa/"
-    },
-    "tumaini_tv": {
-        "name": "Tumaini TV",
-        "url": "https://www.squidtv.net/africa/kenya/kenya-006.html"
-    },
-    "younib_media_tv": {
-        "name": "YOUNIB Media TV",
-        "url": "https://livestreamlinks.net/en/onlinetv/kenya/younib-media-tv"
+    "werutv": {
+        "name": "Weru TV",
+        "url": "https://werutv.co.ke/live/"
     }
-}       
+}

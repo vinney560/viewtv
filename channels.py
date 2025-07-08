db8775bc8d27d806f8165c459407c6f778a5748e
VINNEY560 <vinneyjoy1@gmail.com>
2025-07-08 13:59:37 +0300

    General Updates

--- channels.py
+++ channels.py
@@ -1,14 +1,14 @@
 CUSTOM_CHANNELS = {
     "citizen-tv": {
-        "name": "Citizen TV Kenya",
+        "name": "Citizen TV",
         "url": "https://www.youtube.com/embed/live_stream?channel=UChBQgieUidXV1CmDxSdRm3g&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
     },
     "ktn-news": {
-        "name": "KTN News Kenya",
+        "name": "KTN News",
         "url": "https://www.youtube.com/embed/live_stream?channel=UCKVsdeoHExltrWMuK0hOWmg&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
     },
     "ntv-kenya": {
-        "name": "NTV Kenya",
+        "name": "NTV",
         "url": "https://ntvkenya-live.stream/ntvkenya.m3u8"
     },
     "k24-tv": {
@@ -20,7 +20,7 @@ CUSTOM_CHANNELS = {
         "url": "https://a62dad94.wurl.com/master/f36d25e7e52f1ba8d7e56eb859c636563214f541/UmFrdXRlblRWLWV1X0ZJRkFQbHVzRW5nbGlzaF9ITFM/playlist.m3u8"
     },
     "kbc": {
-        "name": "Kenya Broadcasting Corporation (KBC)",
+        "name": "KBC",
         "url": "https://www.youtube.com/embed/live_stream?channel=UCpl2xr6WWfxa2v2X6tqRmIw&controls=0&modestbranding=1&rel=0&fs=1&disablekb=1&iv_load_policy=3"
     },
     "inooro-tv": {
@@ -39,44 +39,48 @@ CUSTOM_CHANNELS = {
         "name": "Pluto TV Sports",
         "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5f2aa5fce6fa5200070d707b/master.m3u8"
     },
-    "pluto-tv-action-sports": {
-        "name": "Pluto TV Action Sports",
+    "action-sports": {
+        "name": "Action Sports",
         "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5f2aa60650e5940007e6081d/master.m3u8"
     },
     "tubi-sports": {
-        "name": "Tubi Sports (USA only, embed)",
+        "name": "Tubi Sports",
         "url": "https://tubitv.com/live/692524"
     },
-    "pluto-tv-anime-all-day": {
-        "name": "Pluto TV Anime All Day",
+    "anime-all-day": {
+        "name": "Anime All Day",
         "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/603f3dcaa959e70007806d5c/master.m3u8"
     },
-    "pluto-tv-anime-247": {
-        "name": "Pluto TV Anime 24/7",
+    "anime-247": {
+        "name": "Anime 24/7",
         "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5e7d178be6fa520007e28fd4/master.m3u8"
     },
-    "pluto-tv-nick-anime": {
-        "name": "Nick Pluto TV (Family/Cartoons)",
+    "nick-anime": {
+        "name": "Nick",
         "url": "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/6266b182b8d2fc00078d12d3/master.m3u8"
     },
     "pltv-usa-movies": {
-        "name": "Pluto TV USA Movies",
+        "name": "Movies",
         "url": "https://i.mjh.nz/PlutoTV/us.m3u8"
     },
-    "tubi-movies-live": {
-        "name": "Tubi Live Movies",
-        "url": "https://www.apsattv.com/tubi.m3u"
+    "disney_1": {
+        "name": "Disney",
+        "url": "http://168.181.122.234:8000/play/a037/index.m3u8"
     },
+    "dusney_2": {
+        "name": "Disney",
+        "url": "https://fl2.moveonjoy.com/DISNEY_JR/index.m3u8"
+    },    
     "nhk-world-japan": {
         "name": "NHK World Japan",
         "url": "https://nhkwlive-ojp.akamaized.net/hls/live/2003459/nhkwlive-ojp-en/index.m3u8"
     },
-    "nba-youtube": {
-        "name": "NBA TV (YouTube Live)",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCWJ2lWNubArHWmf3FIHbfcQ&controls=0&modestbranding=1"
+    "nickelodeon": {
+        "name": "Nickelodeon",
+        "url": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5d8d08395f39465da6fb3ec4/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5107-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&serverSideAds=false&sid=919bc5fd-6cce-44a6-bb39-2894dea1c988"
     },
-    "cartoon-network-asia": {
-        "name": "Cartoon Network Asia (YouTube Live)",
-        "url": "https://www.youtube.com/embed/live_stream?channel=UCSahQLU1-218c0K9QJPD1oA&controls=0&modestbranding=1"
+    "nick_jr": {
+        "name": "Nick Jr.",
+        "url": "http://cfd-v4-service-channel-stitcher-use1-1.prd.pluto.tv/stitch/hls/channel/5ca6748a37b88b269472dad9/master.m3u8?appName=web&appVersion=unknown&clientTime=0&deviceDNT=0&deviceId=6c2a5106-30d3-11ef-9cf5-e9ddff8ff496&deviceMake=Chrome&deviceModel=web&deviceType=web&deviceVersion=unknown&includeExtendedEvents=false&sid=d95e31ff-c65d-438c-9466-ddbbd097b4cf&serverSideAds=false"
     }
 }        

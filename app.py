f9a0fed01aaa8823645f74b979c7a67dd7aa8f22
VINNEY560 <vinneyjoy1@gmail.com>
2025-07-13 13:03:14 +0300

    General Updates

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ app.config['MAIL_USE_TLS'] = os.getenv("MAIL_USE_TLS") == 'True'
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
-# Current UTC time
+
 now_utc = datetime.utcnow()
 # Nairobi is UTC+3
 now_eat = now_utc + timedelta(hours=3)
